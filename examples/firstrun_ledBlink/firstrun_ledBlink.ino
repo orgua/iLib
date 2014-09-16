6501792dff4bee055b5c6a89832ad602d7ee6914
orgua <splingmael@gmail.com>
2014-09-17 00:26:56 +0200

    added firstrun-script
    inclusive fuse-settings for atmel 328

---
+++ examples/firstrun_ledBlink/firstrun_ledBlink.ino
@@ -0,0 +1,36 @@
+/**<
+
+Program Fuses:
+
+LOW     0xFD
+HIGH    0xDF
+EXT     0xFF
+
+ */
+
+#define TIMEON   20
+#define TIMEOFF  (1000-TIMEON)
+
+#define LEDPINA   8
+#define LEDPINB   11
+
+void setup() {
+  pinMode(LEDPINA, OUTPUT);
+  pinMode(LEDPINB, OUTPUT);
+  digitalWrite(LEDPINA,LOW);
+  digitalWrite(LEDPINB,LOW);
+
+}
+
+void loop() {
+
+  delay(TIMEOFF);
+  digitalWrite(LEDPINA,HIGH);
+  digitalWrite(LEDPINB,HIGH);
+  delay(TIMEON); // busywaiting
+  digitalWrite(LEDPINA,LOW);
+  digitalWrite(LEDPINB,LOW);
+
+}
+
+
