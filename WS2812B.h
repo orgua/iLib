be75fbeed400fa8dc98d891a0eed36142f21f82c
orgua <splingmael@gmail.com>
2014-09-17 00:16:34 +0200

    added Lib for ws2812b

---
+++ WS2812B.h
@@ -0,0 +1,211 @@
+#ifndef WS2812B_h
+#define WS2812B_h
+
+#include "avr/interrupt.h"
+#include "avr/io.h"
+#include "util/delay.h"
+struct cRGB { uint8_t g; uint8_t r; uint8_t b; };
+
+/** ######################################################################
+
+Driver for the WS2812b
+
+ Details:
+
+
+########################################################################  */
+
+class WS2812B
+{
+
+    /** ######### Register-Map ################################################################# */
+
+#define CONCAT(a, b)            a ## b
+#define CONCAT_EXP(a, b)        CONCAT(a, b)
+
+#define ws2812_PORTREG          CONCAT_EXP(PORT,ws2812_port)
+#define ws2812_DDRREG           CONCAT_EXP(DDR,ws2812_port)
+
+#define ws2812_port             B     // Data port
+#define ws2812_pin              1     // Data out pin
+
+
+    /** ######### function definition ################################################################# */
+
+public:
+
+    WS2812B(void)
+    {
+#ifdef __AVR_ATtiny10__
+        CCP=0xD8;		// configuration change protection, write signature
+        CLKPSR=0;		// set cpu clock prescaler =1 (8Mhz) (attiny 4/5/9/10)
+#else
+        CLKPR=_BV(CLKPCE);
+        CLKPR=0;			// set clock prescaler to 1 (attiny 25/45/85/24/44/84/13/13A)
+#endif
+    };
+
+    void inline setleds(struct cRGB *ledarray, uint16_t leds)
+    {
+        setleds_pin(ledarray,leds, _BV(ws2812_pin));
+    }
+
+    void inline setleds_pin(struct cRGB *ledarray, uint16_t leds, uint8_t pinmask)
+    {
+        ws2812_DDRREG |= _BV(ws2812_pin); // Enable DDR
+        sendarray_mask((uint8_t*)ledarray,leds+leds+leds,pinmask);
+        _delay_us(50);
+    }
+
+    void sendarray(uint8_t *data,uint16_t datlen)
+    {
+        sendarray_mask(data,datlen,_BV(ws2812_pin));
+    }
+
+    /*
+      This routine writes an array of bytes with RGB values to the Dataout pin
+      using the fast 800kHz clockless WS2811/2812 protocol.
+    */
+
+// Timing in ns
+#define w_zeropulse   350
+#define w_onepulse    900
+#define w_totalperiod 1250
+
+// Fixed cycles used by the inner loop
+#define w_fixedlow    2
+#define w_fixedhigh   4
+#define w_fixedtotal  8
+
+// Insert NOPs to match the timing, if possible
+#define w_zerocycles    (((F_CPU/1000)*w_zeropulse          )/1000000)
+#define w_onecycles     (((F_CPU/1000)*w_onepulse    +500000)/1000000)
+#define w_totalcycles   (((F_CPU/1000)*w_totalperiod +500000)/1000000)
+
+// w1 - nops between rising edge and falling edge - low
+#define w1 (w_zerocycles-w_fixedlow)
+// w2   nops between fe low and fe high
+#define w2 (w_onecycles-w_fixedhigh-w1)
+// w3   nops to complete loop
+#define w3 (w_totalcycles-w_fixedtotal-w1-w2)
+
+#if w1>0
+#define w1_nops w1
+#else
+#define w1_nops  0
+#endif
+
+// The only critical timing parameter is the minimum pulse length of the "0"
+// Warn or throw error if this timing can not be met with current F_CPU settings.
+#define w_lowtime ((w1_nops+w_fixedlow)*1000000)/(F_CPU/1000)
+#if w_lowtime>550
+#error "Light_ws2812: Sorry, the clock speed is too low. Did you set F_CPU correctly?"
+#elif w_lowtime>450
+#warning "Light_ws2812: The timing is critical and may only work on WS2812B, not on WS2812(S)."
+#warning "Please consider a higher clockspeed, if possible"
+#endif
+
+#if w2>0
+#define w2_nops w2
+#else
+#define w2_nops  0
+#endif
+
+#if w3>0
+#define w3_nops w3
+#else
+#define w3_nops  0
+#endif
+
+#define w_nop1  "nop      \n\t"
+#define w_nop2  "rjmp .+0 \n\t"
+#define w_nop4  w_nop2 w_nop2
+#define w_nop8  w_nop4 w_nop4
+#define w_nop16 w_nop8 w_nop8
+
+    void inline sendarray_mask(uint8_t *data,uint16_t datlen,uint8_t maskhi)
+    {
+        uint8_t curbyte,ctr,masklo;
+        uint8_t sreg_prev;
+
+        masklo	=~maskhi&ws2812_PORTREG;
+        maskhi |=        ws2812_PORTREG;
+        sreg_prev=SREG;
+        cli();
+
+        while (datlen--)
+        {
+            curbyte=*data++;
+
+            asm volatile(
+                "       ldi   %0,8  \n\t"
+                "loop%=:            \n\t"
+                "       out   %2,%3 \n\t"    //  '1' [01] '0' [01] - re
+#if (w1_nops&1)
+                w_nop1
+#endif
+#if (w1_nops&2)
+                w_nop2
+#endif
+#if (w1_nops&4)
+                w_nop4
+#endif
+#if (w1_nops&8)
+                w_nop8
+#endif
+#if (w1_nops&16)
+                w_nop16
+#endif
+                "       sbrs  %1,7  \n\t"    //  '1' [03] '0' [02]
+                "       out   %2,%4 \n\t"    //  '1' [--] '0' [03] - fe-low
+                "       lsl   %1    \n\t"    //  '1' [04] '0' [04]
+#if (w2_nops&1)
+                w_nop1
+#endif
+#if (w2_nops&2)
+                w_nop2
+#endif
+#if (w2_nops&4)
+                w_nop4
+#endif
+#if (w2_nops&8)
+                w_nop8
+#endif
+#if (w2_nops&16)
+                w_nop16
+#endif
+                "       out   %2,%4 \n\t"    //  '1' [+1] '0' [+1] - fe-high
+#if (w3_nops&1)
+                w_nop1
+#endif
+#if (w3_nops&2)
+                w_nop2
+#endif
+#if (w3_nops&4)
+                w_nop4
+#endif
+#if (w3_nops&8)
+                w_nop8
+#endif
+#if (w3_nops&16)
+                w_nop16
+#endif
+
+                "       dec   %0    \n\t"    //  '1' [+2] '0' [+2]
+                "       brne  loop%=\n\t"    //  '1' [+3] '0' [+4]
+                :	"=&d" (ctr)
+                :	"r" (curbyte), "I" (_SFR_IO_ADDR(ws2812_PORTREG)), "r" (maskhi), "r" (masklo)
+            );
+        }
+
+        SREG=sreg_prev;
+    }
+
+
+};
+
+/** ######### Preinstantiate Object ################################################################# */
+/** < it's better when this is done by the user */
+//PRESET preset = PRESET();
+
+#endif
---
+++ examples/WS2812B_Clock_90Led/WS2812B_Clock_90Led.ino
@@ -0,0 +1,184 @@
+#include "WS2812B.h"
+WS2812B ws2812b = WS2812B();
+
+#define LEDS_SIZE     90 //120
+#define LEDS_CHANNELS  3
+#define LEDS_DELAY    55
+#define LEDS_DIST     30 //(LEDS_SIZE/LEDS_CHANNELS)
+#define LEDS_INCR     1 //12 //(255/(LEDS_SIZE/LEDS_CHANNELS))
+#define LEDS_MAX      LEDS_INCR*LEDS_DIST
+struct cRGB led[LEDS_SIZE];
+
+volatile uint8_t selected_led, actual_led, actual_channel, actual_status;
+
+struct time {
+   uint8_t tv_hour;
+   uint8_t tv_min;
+   uint8_t tv_sec;
+   uint16_t tv_msec;
+} clock;
+
+struct timeA {
+   uint16_t tv_hour;
+   uint16_t tv_min;
+   uint16_t tv_sec;
+} analog;
+
+#define SCALEMAX   12000
+
+#define SCALEHA    (SCALEMAX / 24) // 500
+#define SCALEHB    (SCALEHA / 60) // 8.33
+#define SCALEMA    (SCALEMAX / 60)
+#define SCALEMB    (SCALEMA / 60)
+#define SCALESA    (SCALEMAX / 60) // 200
+#define SCALESB    (SCALESA / 1000) // 0.2
+#define SCALESBi   (1000 / SCALESA) // 5
+#define SCALELED   (SCALEMAX / LEDS_SIZE)
+
+uint32_t milli_old;
+
+void get_analog() {
+  uint32_t milli_new = millis();
+  uint32_t tv_msec = clock.tv_msec + (milli_new - milli_old);
+  milli_old     = milli_new;
+  uint32_t tv_sec  = clock.tv_sec + (tv_msec/1000);
+  uint32_t tv_min  = clock.tv_min + (tv_sec / 60);
+  uint32_t tv_hour = clock.tv_hour + (tv_min / 60);
+  /*
+  uint8_t  tv_day  = (tv_hour / 24);
+  clock.tv_hour = tv_hour - tv_day * 24;
+  clock.tv_min  = tv_min  - tv_hour* 60;
+  clock.tv_sec  = tv_sec  - tv_min * 60;
+  clock.tv_msec = tv_msec - tv_sec * 1000;
+  */
+  while (tv_hour > 23)  tv_hour -= 24;
+  clock.tv_hour = tv_hour;
+  while (tv_min > 59)   tv_min -= 60;
+  clock.tv_min = tv_min;
+  while (tv_sec > 59)   tv_sec -= 60;
+  clock.tv_sec = tv_sec;
+  while (tv_msec > 999) tv_msec -= 1000;
+  clock.tv_msec = tv_msec;
+
+
+  // zahlen normiert
+  analog.tv_hour = (clock.tv_hour*SCALEHA) + (clock.tv_min*SCALEHB);
+  if (analog.tv_hour >= SCALEMAX)   analog.tv_hour -= SCALEMAX;
+  analog.tv_min  = (clock.tv_min*SCALEMA) + (clock.tv_sec*SCALEMB);
+  if (analog.tv_min >= SCALEMAX)    analog.tv_min -= SCALEMAX;
+  analog.tv_sec  = (clock.tv_sec*SCALESA) + (clock.tv_msec/SCALESBi);
+  if (analog.tv_sec >= SCALEMAX)    analog.tv_sec -= SCALEMAX;
+}
+
+
+void setup(void) {
+  clock.tv_hour = 3;
+  clock.tv_min  = 45;
+  clock.tv_sec  = 50;
+  clock.tv_msec = 0;
+}
+
+
+void loop(void)
+{
+
+  uint8_t red, green, blue;
+  uint8_t ledi;
+
+  red   = LEDS_MAX;
+  green = 0;
+  blue  = 0;
+
+  while(1)
+  {
+
+    // Farbkreis - Algo
+    ledi = actual_led;
+    for (uint8_t x=0; x<LEDS_SIZE; x++) {
+
+      if (x < LEDS_DIST)         { blue  = 0; red   = red   - LEDS_INCR; green = green + LEDS_INCR; }
+      else if (x < 2*LEDS_DIST)  { red   = 0; green = green - LEDS_INCR; blue  = blue  + LEDS_INCR; }
+      else                       { green = 0; blue  = blue  - LEDS_INCR; red   = red   + LEDS_INCR; }
+
+      #define GROUNDLIGHT 0
+      #define SHIFTFACTOR   0
+      led[ledi].r = GROUNDLIGHT + (red>>SHIFTFACTOR);
+      led[ledi].g = GROUNDLIGHT + (green>>SHIFTFACTOR);
+      led[ledi].b = GROUNDLIGHT + (blue>>SHIFTFACTOR);
+
+      if (++ledi >= LEDS_SIZE) ledi = 0;
+    }
+    //if (++actual_led>=LEDS_SIZE) { actual_led = 0; }  // Clockwise Color-Ring
+    if (actual_led--==0)  actual_led = LEDS_SIZE - 1;  // Reverse Color-Ring
+
+    // Uhr einfügen
+    #define CLOCK_INTENSITY 255
+    #define CLOCK_WIDTH     (1)
+
+    uint8_t  led_mid, led_sel, led_overflow;
+    int16_t  value;
+    uint16_t analog_LED;
+
+    get_analog();
+
+    //if (led[analog.tv_hour/SCALELED].r < CLOCK_INTENSITY) { led[analog.tv_hour/SCALELED].r = CLOCK_INTENSITY; }
+    //if (led[analog.tv_min/SCALELED].g < CLOCK_INTENSITY) { led[analog.tv_min/SCALELED].g = CLOCK_INTENSITY; }
+
+     // HOURS
+    led_mid = (analog.tv_hour/SCALELED);
+    led_overflow = 0;
+    if (led_mid >= CLOCK_WIDTH) { led_sel = led_mid - CLOCK_WIDTH; }
+
+    else                        { led_sel = led_mid + (LEDS_SIZE - CLOCK_WIDTH); analog.tv_hour += SCALEMAX; }
+    for (uint8_t x=0; x<=2*CLOCK_WIDTH; x++) {
+      if (led_overflow)               { analog_LED = (led_sel + LEDS_SIZE) * SCALELED; }
+        else                          { analog_LED = led_sel * SCALELED; }
+      if (analog_LED > analog.tv_hour) { value = (analog_LED - analog.tv_hour); }
+        else                          { value = (analog.tv_hour - analog_LED); }
+      if (value > CLOCK_INTENSITY)    { value = 0; }
+        else                          { value = CLOCK_INTENSITY - value; }
+      if (led[led_sel].r < value)     { led[led_sel].r = value; }  // intensity normal
+      if (++led_sel >= LEDS_SIZE)     { led_sel -= LEDS_SIZE; led_overflow = 1;}
+    }
+
+    // MINUTES
+    led_mid = (analog.tv_min/SCALELED);
+    led_overflow = 0;
+    if (led_mid >= CLOCK_WIDTH) { led_sel = led_mid - CLOCK_WIDTH; }
+    else                        { led_sel = led_mid + (LEDS_SIZE - CLOCK_WIDTH); analog.tv_min += SCALEMAX; }
+
+    for (uint8_t x=0; x<=2*CLOCK_WIDTH; x++) {
+      if (led_overflow)               { analog_LED = (led_sel + LEDS_SIZE) * SCALELED; }
+        else                          { analog_LED = led_sel * SCALELED; }
+      if (analog_LED > analog.tv_min) { value = (analog_LED - analog.tv_min); }
+        else                          { value = (analog.tv_min - analog_LED); }
+      if (value > CLOCK_INTENSITY)    { value = 0; }
+        else                          { value = CLOCK_INTENSITY - value; }
+      if (led[led_sel].g < value)     { led[led_sel].g = value; }  // intensity normal
+      if (++led_sel >= LEDS_SIZE)     { led_sel -= LEDS_SIZE; led_overflow = 1;}
+    }
+
+    // SECONDS
+    led_mid = (analog.tv_sec/SCALELED);
+    led_overflow = 0;
+    if (led_mid >= CLOCK_WIDTH) { led_sel = led_mid - CLOCK_WIDTH; }
+    else                        { led_sel = led_mid + (LEDS_SIZE - CLOCK_WIDTH); analog.tv_sec += SCALEMAX; }
+
+    for (uint8_t x=0; x<=2*CLOCK_WIDTH; x++) {
+      if (led_overflow)               { analog_LED = (led_sel + LEDS_SIZE) * SCALELED; }
+        else                          { analog_LED = led_sel * SCALELED; }
+      if (analog_LED > analog.tv_sec) { value = (analog_LED - analog.tv_sec); }
+        else                          { value = (analog.tv_sec - analog_LED); }
+      if (value > CLOCK_INTENSITY)    { value = 0; }
+        else                          { value = CLOCK_INTENSITY - value; }
+      if (led[led_sel].b < value)     { led[led_sel].b = value; }  // intensity normal
+      if (++led_sel >= LEDS_SIZE)     { led_sel -= LEDS_SIZE; led_overflow = 1;}
+    }
+
+
+    //led[0].r=255;led[0].g=0;led[0].b=0;    // Write red to array
+    ws2812b.setleds(led,LEDS_SIZE);
+    _delay_ms(LEDS_DELAY);                         // wait for 500ms.
+
+  }
+}
