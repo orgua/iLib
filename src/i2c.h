0082d8b9c4b2562e886b866d73abaa38b5fa4bc1
Ingmar Inʒo Splitt <orgua@users.noreply.github.com>
2022-11-20 01:03:51 +0100

    Update i2c.h
    unify naming style

--- src/i2c.h
+++ src/i2c.h
@@ -118,10 +118,10 @@ void WirePlus::writeCMD(const uint8_t address, const uint8_t cmd)
     Wire.endTransmission();
 };
 
-void WirePlus::read(const uint8_t address, const uint8_t registeraddress, uint8_t buff[], const uint8_t length=1)
+void WirePlus::read(const uint8_t address, const uint8_t register_address, uint8_t buffer[], const uint8_t length=1)
 {
     Wire.beginTransmission(address); 	// Address + WRITE (0)
-    Wire.write(registeraddress);
+    Wire.write(register_address);
     Wire.endTransmission(false); 		// No Stop Condition, for repeated Talk
 
     if (!length) return;
@@ -130,11 +130,9 @@ void WirePlus::read(const uint8_t address, const uint8_t registeraddress, uint8_
     _i=0;
     while(Wire.available())
     {
-        buff[_i] = Wire.read();
+        buffer[_i] = Wire.read();
         _i++;
     }
-
-    Wire.endTransmission(true); 		// Stop Condition
 };
 
 uint8_t WirePlus::readByte(const uint8_t address, const uint8_t register_address)
@@ -144,19 +142,19 @@ uint8_t WirePlus::readByte(const uint8_t address, const uint8_t register_address
     return _readvalue;
 };
 
-void WirePlus::setRegister(const uint8_t address, const uint8_t registeraddress, const uint8_t mask, const uint8_t writevalue)
+void WirePlus::setRegister(const uint8_t address, const uint8_t register_address, const uint8_t mask, const uint8_t write_value)
 {
     uint8_t _setting;
-    read(address, registeraddress, &_setting, 1 );
+    read(address, register_address, &_setting, 1 );
     _setting     &= ~mask;
-    _setting     |= (writevalue&mask);
-    writeByte(address, registeraddress, _setting);
+    _setting     |= (write_value&mask);
+    writeByte(address, register_address, _setting);
 };
 
-uint8_t WirePlus::getRegister(const uint8_t address, const uint8_t registeraddress, const uint8_t mask)
+uint8_t WirePlus::getRegister(const uint8_t address, const uint8_t register_address, const uint8_t mask)
 {
     uint8_t _setting;
-    read(address, registeraddress, &_setting, (uint8_t)1 );
+    read(address, register_address, &_setting, (uint8_t)1 );
     return (_setting & mask);
 };
 
